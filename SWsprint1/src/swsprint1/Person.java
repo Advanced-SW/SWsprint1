a5022f24954b74347ed7db00d0dcb871243644ea
Nourin <nourin.osama55@gmail.com>
2021-11-24 20:29:23 +0200

    implement class person

---
+++ SWsprint1/src/swsprint1/Person.java
@@ -0,0 +1,48 @@
+public class Person {
+    protected String name;
+    protected String password;
+    protected String mobile;
+    protected String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", mobile='" + mobile + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
