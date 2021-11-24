0b5117a02c2143d6ad264aa32d953aa913c1509e
ZainabGhanem <93927371+ZainabGhanem@users.noreply.github.com>
2021-11-24 20:55:11 +0200

    request class

---
+++ SWsprint1/src/swsprint1/Request.java
@@ -0,0 +1,13 @@
+public class Request {
+    private User client;
+    private  Area area;
+public Area getArea()
+{
+    return  area;
+}
+public void setData(User client,Area area)
+{
+    this.client=client;
+    this.area=area;
+}
+}
